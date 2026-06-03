31e6176998683984419b67fddec5ac945d40e1aa
shutema <30980722+shutema@users.noreply.github.com>
2026-06-03 15:58:59 +0200

    Update NrfRadio.h Longer gap between power change and commit

--- src/hm/NrfRadio.h
+++ src/hm/NrfRadio.h
@@ -348,7 +348,7 @@ class NrfRadio : public Radio {
                                     isLastPackage = true;                                // response from dev control command
                                     // --- CUSTOM GAP INSERTION By MS ---
                                     // Give the Gen 1 inverter time to process the data block before we fire the close command
-                                    delay(50); 
+                                    delay(250); 
                             }
                             rx_ready = true; //reset in case we first read messages from other inverter or ACK zero payloads
                         }
